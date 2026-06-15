83b15d99bac1ea2a630683884cc684753da68cee
James P Jackson <jacksonjp0311@gmail.com>
2026-06-15 04:04:47 -0400

    Align latest pointer to v0.8 seal commit

--- tests/test_v06_repair_replay.py
+++ tests/test_v06_repair_replay.py
@@ -10,7 +10,7 @@ from asf.repair.repair_replay import replay_repair
 
 
 ROOT = Path(__file__).resolve().parents[1]
-REMOTE_HEAD = "efeca6a073608a12f1a3bbadf09c414cc1c24a3b"
+REMOTE_HEAD = "fc0eba819de9e1b8660ac7cfe3d0c9a2bcc88f9f"
 
 
 def plan() -> RepairPlan:
